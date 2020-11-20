db071687ae0feee2f72e6cf8c05b8eb2e8892b8f
ElWazy <santiago.fierro4@outlook.cl>
2020-11-20 08:28:35 -0300

    Version 1.0 del proyecto terminado
    obviamente faltan versione de parche porque faltan funcionalidades, pero
    la base ya esta hecha

--- src/org/santotomas/library_app/MenuLibreria.java
+++ src/org/santotomas/library_app/MenuLibreria.java
@@ -14,6 +14,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -65,6 +66,7 @@ public class MenuLibreria extends JFrame implements ActionListener {
     private JTextField txtSemana3;
     private JTextField txtSemana4;
     private JButton btnGraficar;
+    private JButton btnActualizarTabla;
     // endregion
 
     private Database myDatabase;
@@ -98,23 +100,25 @@ public class MenuLibreria extends JFrame implements ActionListener {
         tbdHome.setForegroundAt(3, Color.decode("#ffffff"));
 
 
-
         String contraSantiago = "1324";
         String contraGaston = "";
-        myDatabase = new Database("localhost", "library", "root", contraGaston);
+        myDatabase = new Database("localhost", "library", "root", contraSantiago);
 
         // region Buttons & Mnemonics
         tbdHome.setMnemonicAt(0, KeyEvent.VK_1);
         tbdHome.setMnemonicAt(1, KeyEvent.VK_2);
         tbdHome.setMnemonicAt(2, KeyEvent.VK_3);
+        tbdHome.setMnemonicAt(3, KeyEvent.VK_4);
 
         btnAgregar.setMnemonic(KeyEvent.VK_A);
         btnActualizar.setMnemonic(KeyEvent.VK_A);
+        btnActualizarTabla.setMnemonic(KeyEvent.VK_T);
         btnEliminar.setMnemonic(KeyEvent.VK_E);
 
         btnAgregar.addActionListener(this);
         btnActualizar.addActionListener(this);
         btnEliminar.addActionListener(this);
+        btnActualizarTabla.addActionListener(this);
 
         // region Radio Buttons Group
         bgCategorias = new ButtonGroup();
@@ -302,6 +306,24 @@ public class MenuLibreria extends JFrame implements ActionListener {
                 btnGraficar.setForeground(Color.decode("#000000"));
             }
         });
+
+        //Color boton Actualizar
+        btnActualizarTabla.setBackground(Color.decode("#f57f17"));
+        btnActualizarTabla.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                btnGraficar.setBackground(Color.decode("#ffb04c"));
+                btnGraficar.setForeground(Color.decode("#000000"));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                btnGraficar.setBackground(Color.decode("#f57f17"));
+                btnGraficar.setForeground(Color.decode("#000000"));
+            }
+        });
     }
 
     public void updateTable() throws SQLException {
@@ -309,7 +331,7 @@ public class MenuLibreria extends JFrame implements ActionListener {
         List<Book> books = bookDAO.getAll();
 
         for (int i = dtmLibros.getRowCount(); i > 0; i--) {
-            dtmLibros.removeRow(i);
+            dtmLibros.removeRow(i - 1);
         }
 
         for (Book book : books) {
@@ -394,11 +416,53 @@ public class MenuLibreria extends JFrame implements ActionListener {
         }
 
         if ( e.getSource() == btnActualizar) {
-            new UpdateBook();
+            Book book = new Book(
+                    String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 0)),
+                    String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 1)),
+                    String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 2)),
+                    Integer.parseInt(String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 3))),
+                    Integer.parseInt(String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 4))),
+                    String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 5)),
+                    String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 6)),
+                    Integer.parseInt(String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 7))),
+                    Date.valueOf(String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 8)))
+            );
+
+            try {
+                new UpdateBook(book);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException classNotFoundException) {
+                classNotFoundException.printStackTrace();
+            }
         }
 
         if (e.getSource() == btnEliminar) {
-            JOptionPane.showConfirmDialog(this, "Está seguro,", "Eliminar libro", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+            String uuid = String.valueOf(dtmLibros.getValueAt(table1.getSelectedRow(), 0));
+            int option = JOptionPane.showConfirmDialog(this, "Está seguro,", "Eliminar libro", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+
+            if (option == JOptionPane.YES_OPTION) {
+                BookDAO bookDAO = new BookDAO(myDatabase);
+                try {
+                    bookDAO.delete(uuid);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+
+            try {
+                updateTable();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if ( e.getSource() == btnActualizarTabla ) {
+            try {
+                updateTable();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
 
         if ( e.getSource() == btnCloseSession ) {
